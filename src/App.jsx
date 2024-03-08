2a892a5cbcd5c0c77faf822744130a8d6e93b6a5
rameezraja0 <rameezraja1730482@gmail.com>
2024-03-08 23:31:21 +0500

    working on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleCheck, faPen, faTrashCan
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
 
   return (
     <>
-      <div className='div'>
-        bdhsbsbkjsd jsdj
+      <div className='container '>
+        <br /><br />
+        <h2>To Do list (ReactJS)</h2>
+        <br /><br />
       </div>
         
     </>
