d887ea45952fbbcc9abf3f35510d5203e76a1e80
rameezraja0 <rameezraja1730482@gmail.com>
2024-03-14 02:59:14 +0500

    Underprocessing

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,78 @@ import {
 
 function App() {
 
+  //Task ToDo List State
+  const [toDo, setToDo] = useState([
+    {id: 1, "title": "Task 1", "status": true},
+    {id: 2, title: "Task 2", "status": false},
+  ])
+  //Temp State
+  const [newTask, setNewTask] = useState('');
+  const [updateData, setUpdateData] = useState('');
+
+  //Add Task
+  const addTask = (id) => {
+
+  }
+  //
+
+  //Add Task
+  const deleteTask = (id) => {
+
+  }
+  //
+  //Add Task
+  const markDone= (id) => {
+
+  }
+  //
+  //Add Task
+  const cancelUpdate= () => {
+
+  }
+  //
+  //Add Task
+  const changeTask= (e) => {
+
+  }
+  //
+
+  //Add Task
+  const updateTask= () => {
+
+  }
+  //
+
   return (
     <>
-      <div className='container '>
+      <div className='container App'>
         <br /><br />
         <h2>To Do list (ReactJS)</h2>
         <br /><br />
-      </div>
         
+        {/* TodoTasks */}
+        {toDo && toDo.length ? '' : 'No Tasks....'}
+
+        {toDo && toDo
+        .map( (task, index) =>{
+          return(
+            <React.Fragment key={task.id}>
+
+              <div className="col taskBg">
+                <div className={task.status ? 'done': ''}>
+              <span className="taskNumber">{index + 1}</span>
+              <span className="taskTest">{task.title}</span>
+
+                </div>
+              </div>
+
+            </React.Fragment>
+          )
+        }
+        )}
+      </div>
+
+
     </>
   )
 }
